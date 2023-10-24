d6f227ea077d2c076569dff05addfba72f1193e5
Sean Stayte <145015624+sean-stayte@users.noreply.github.com>
2023-10-24 11:53:53 +0100

    Add files via upload

---
+++ ticTacToe.py
@@ -0,0 +1,40 @@
+theBoard = {'top-L': ' ', 'top-M': ' ', 'top-R': ' ',
+            'mid-L': ' ', 'mid-M': ' ', 'mid-R': ' ',
+            'low-L': ' ', 'low-M': ' ', 'low-R': ' '}
+
+def printBoard(board):
+    print(board['top-L'] + '|' + board['top-M'] + '|' + board['top-R'])
+    print('-+-+-')
+    print(board['mid-L'] + '|' + board['mid-M'] + '|' + board['mid-R'])
+    print('-+-+-')
+    print(board['low-L'] + '|' + board['low-M'] + '|' + board['low-R'])
+
+turn = 'X'
+for i in range(9):
+    if theBoard['top-L'] == theBoard['top-M'] == theBoard['top-R'] and theBoard['top-L'] != ' ':
+        print(theBoard['top-L'] + ' wins!')
+    elif theBoard['mid-L'] == theBoard['mid-M'] == theBoard['mid-R'] and theBoard['mid-L'] != ' ':
+        print(theBoard['mid-L'] + ' wins!')
+    elif theBoard['low-L'] == theBoard['low-M'] == theBoard['low-R'] and theBoard['low-L'] != ' ':
+        print(theBoard['low-L'] + ' wins!')
+    elif theBoard['top-L'] == theBoard['mid-L'] == theBoard['low-L'] and theBoard['top-L'] != ' ':
+        print(theBoard['top-L'] + ' wins!')
+    elif theBoard['top-M'] == theBoard['mid-M'] == theBoard['low-M'] and theBoard['top-M'] != ' ':
+        print(theBoard['top-M'] + ' wins!')
+    elif theBoard['top-R'] == theBoard['mid-R'] == theBoard['low-R'] and theBoard['top-R'] != ' ':
+        print(theBoard['top-R'] + ' wins!')
+    elif theBoard['top-L'] == theBoard['mid-M'] == theBoard['low-R'] and theBoard['top-L'] != ' ':
+        print(theBoard['top-L'] + ' wins!')
+    elif theBoard['top-R'] == theBoard['mid-M'] == theBoard['low-L'] and theBoard['top-R'] != ' ':
+        print(theBoard['top-L'] + ' wins!')
+    else:
+        printBoard(theBoard)
+        print('Turn for ' + turn + '. Move on which space?')
+        move = input()
+        theBoard[move] = turn
+        if turn == 'X':
+            turn = 'O'
+        else:
+            turn = 'X'
+printBoard(theBoard)
+
